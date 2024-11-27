fbb04110d3feb7c3c8ee2ac6c8084b189ca57973
TinHtunOo <tinhtunhe@gmail.com>
2024-11-27 17:27:21 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import RisingStar from "./RisingStar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RisingStar maxRating={10} />
+    <RisingStar maxRating={20} color="blue" size={34} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
